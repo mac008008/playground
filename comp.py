1734b803cc1f2281f43b579f129b1a03104bd0cc
Makoto Oya <mac008008@gmail.com>
2016-04-22 03:37:35 +0000

    initialized

---
+++ comp.py
@@ -0,0 +1,13 @@
+scores = [45, 200, 55, 348]
+sum = 0
+i = 0
+for score in scores:
+	sum += score
+else:
+	print sum
+
+for i in range(10):
+	if i==3:
+		continue
+	print i
+
---
+++ hello.py
@@ -0,0 +1,11 @@
+a = 100
+b = [22, 33, 444, 2, 85, 4]
+family = ["tasuke", "sosuke", "ayami", "makoto"]
+sales = {"oya":50, "tanaka":30, "suzuki":120}
+b.sort()
+b.reverse()
+print b
+family.reverse()
+print family
+print sales["tanaka"]
+# print("a = %5d") % a
